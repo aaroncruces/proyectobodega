047280c1ecb5f9c8b0ce3037c4cd96c94e9334f3
aaron cruces <aaroncruces@gmail.com>
2021-07-26 17:21:59 -0400

    separacion logica

--- src_cliente/components/Inputbox_ubicacion.tsx
+++ src_cliente/components/Inputbox_ubicacion.tsx
@@ -4,9 +4,10 @@ import Props_inputbox from "../helpers/type_props_Inputbox";
 import { onInput, onBlur } from "../helpers/formato_descripciones";
 import StateUbicacion from "../redux/ubicacion/type_state_ubicacion";
 import { setUbicacion } from "../redux/ubicacion/ubicacionActionCreators";
+import { ubicacionFromState } from "../redux/StateValueExtractor";
 
 const mapStateToProps = (state): Props_inputbox => ({
-  textInputBox: (state.ubicacionReducer as StateUbicacion).ubicacion,
+  textInputBox: ubicacionFromState(state),
   name: "ubicacion",
   labelBody: "Ubicacion",
   format_onBlur: onBlur,
